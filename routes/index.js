3d51ba452e42357f5802d4b0d02a18307d15006e
Shahriar Islam <theshahriarislam@gmail.com>
2022-10-13 09:04:49 +0600

    edited feature

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const storeRoutes = require("./store");
-const hrRoutes = require("./hr")
+const hrRoutes = require("./hr");
+
+const { manageProducts } = require("./../controllers/store/product");
+
+router.get("/test", manageProducts);
 
 router.use("/store", storeRoutes);
 router.use("/hr", hrRoutes);
-
 
 module.exports = router;
