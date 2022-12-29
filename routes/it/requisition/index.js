5de0d2376dc5407e89c7df95c5e5616ef5b105ed
saifakib <mdsaifakib@gmail.com>
2022-12-29 15:55:45 +0600

    issue: error resolve in routes

--- routes/it/requisition/index.js
+++ routes/it/requisition/index.js
@@ -9,13 +9,15 @@ router.put("/approve", checkApproveRequisition, putReqByItStoreOfficer);
 router.put("/deny", checkDenyRequisition, denyRequisition);
 router.put("/accept", checkAcceptRequisition, acceptUserRequisition);
 
+router.get("/admin", getAdminRequisitions);
+
+router.get("/admin/:req_id", getRequsition)
+
 router.get("/user/accReq", checkUserAcceptRequisition, getUserAcceptRequitions);
 router.get("/user/accReq/active", checkUserAcceptRequisition, getUserAcceptActiveRequitions);
 router.get("/user/:user_id", checkUserRequisition, getUserRequitions);
 router.get("/:req_id", getUserAcceptRequition);
 
-router.get("/admin", getAdminRequisitions);
 
-router.get("/admin/:req_id", getRequsition)
 
 module.exports = router;
